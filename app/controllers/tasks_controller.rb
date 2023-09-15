5d12968c2772620a6eaf8276f2aeff28a6060eb1
Shama <bernardshammah@gmail.com>
2023-09-15 16:54:17 +0300

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+# app/controllers/tasks_controller.rb
+
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: 'Task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
+end
